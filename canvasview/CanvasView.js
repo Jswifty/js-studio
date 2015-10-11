43491a5826e66c2ba5307113a940a9e98ae5d404
Jswifty <wongjohnwwc@yahoo.com.hk>
2015-10-11 13:31:17 +0100

    Update CanvasView
    created CanvasViewListener for listening canvas pause and resume.
    remove Mouse instance because it’s not an essential object within
    CanvasView

--- canvasview/CanvasView.js
+++ canvasview/CanvasView.js
@@ -1,13 +1,15 @@
 /**  
  *	CanvasView.js is an object class which creates an instance of a HTML5 canvas
  *	under the given DIV container.
- *	It also initiates a mouse and an animator, which it can pass MouseListeners and renderFunctions into them.
+ *	It also initiates an animator, which it can pass renderFunctions through into it.
  *
- *	Required: Mouse.js, Animator.js
+ *	Required: Animator.js
  */ 
 
 var CanvasView = function (divContainer) {
-
+	
+	var canvasView = this;
+	
 	/* Store the DIV container. */
 	this.divContainer = divContainer;
 	
@@ -16,91 +18,108 @@ var CanvasView = function (divContainer) {
 	this.canvas.style.position = "absolute";
 	this.canvas.innerHTML = "Your browser does not support HTML5 canvas.";
 	
-	/* Setup the Mouse. */
-	this.mouse = new Mouse(this.divContainer);
-	
-	/* Setup the Animator. */
+	/* Create an Animator instance. */
 	this.animator = new Animator();
+	
+	/* The list of canvas view listeners this view is appended to. Each canvas view event will trigger the corresponding method of each listeners. */
+	this.listeners = [];
 	
 	/** Canvas width getter. */
 	this.getCanvasWidth = function () {
-		return this.canvas.width;
+		return canvasView.canvas.width;
 	};
 
 	/** Canvas height getter. */
 	this.getCanvasHeight = function () {
-		return this.canvas.height;
+		return canvasView.canvas.height;
 	};
 	
 	/** Canvas context getter. */
 	this.getCanvas2DContext = function () {
-		return this.canvas.getContext("2d");
+		return canvasView.canvas.getContext("2d");
 	};
 	
 	/** Perform action for window resize event. */
 	this.onResize = function () {
 
 		/* Set canvas dimensions. */
-		this.canvas.width = this.divContainer.offsetWidth; 
-		this.canvas.height = this.divContainer.offsetHeight;
+		canvasView.canvas.width = canvasView.divContainer.offsetWidth; 
+		canvasView.canvas.height = canvasView.divContainer.offsetHeight;
 	};
 	
 	/** Add a drawing method to the animator. */
 	this.addRenderFunction = function (renderFunction) {
-		return this.animator.addRenderFunction(this, renderFunction);
+		return canvasView.animator.addRenderFunction(canvasView, renderFunction);
 	};
 	
 	/** Remove a drawing method from the animator. */
 	this.removeRenderFunction = function (renderID) {
-		this.animator.removeRenderFunction(renderID);
-	};
-	
-	/** Add a mouse listener to the mouse. */
-	this.addMouseListener = function (mouseListener) {
-		this.mouse.addMouseListener(mouseListener);
-	};
-	
-	/** Remove a mouse listener from the mouse. */
-	this.removeMouseListener = function (mouseListener) {
-		this.mouse.removeMouseListener(mouseListener);
+		canvasView.animator.removeRenderFunction(renderID);
 	};
 	
 	/** Call the animator to pause the animation. */
 	this.pause = function () {
-		this.animator.pause();
+		canvasView.animator.pause();
 	};
 	
 	/** Call the animator to resume the animation. */
 	this.resume = function () {
-		this.animator.resume();
+		canvasView.animator.resume();
+	};
+
+	/** Add a canvas view listener to the canvas view . */
+	this.addCanvasViewListener = function (canvasViewListener) {
+
+		/* Make sure the input object qualifies as an instance of CanvasViewListener. */
+		if (!canvasViewListener.onCanvasViewPause || typeof canvasViewListener.onCanvasViewPause !== "function") {
+			canvasViewListener.onCanvasViewPause = function() {};
+		}
+
+		if (!canvasViewListener.onCanvasViewResume || typeof canvasViewListener.onCanvasViewResume !== "function") {
+			canvasViewListener.onCanvasViewResume = function() {};
+		}
+
+		canvasView.listeners.push(canvasViewListener);
+	};
+	
+	/** Remove a canvas view listener to the canvas view. */
+	this.removeCanvasViewListener = function (canvasViewListener) {
+
+		/* Attempt to find the index of the given listener and then remove it. */
+		for (var i = canvasView.listeners.length - 1; i >= 0; i--) {
+			if (canvasView.listeners[i] === canvasViewListener) {
+				canvasView.listeners.splice(i, 1);
+			}
+		}
 	};
 	
 	/** Perform action for animator pause event.
 	 *	This will be called when calling pause() as well. */
-	this.onViewPause = function () {
+	this.onPause = function () {
+		for (var i = 0; i < canvasView.listeners.length; i++) {
+			canvasView.listeners[i].onCanvasViewPause();
+		}
 		console.log("Canvas view paused at: " + new Date().toString());
 	};
 
 	/** Perform action for animator pause event.
 	 *	This will be called when calling resume() as well. */
-	this.onViewResume = function () {
+	this.onResume = function () {
+		for (var i = 0; i < canvasView.listeners.length; i++) {
+			canvasView.listeners[i].onCanvasViewResume();
+		}
 		console.log("Canvas view resume at: " + new Date().toString());
 	};
-	
-	/**** INITIALISATION ****/
-	var view = this;
 	
 	/* Append the canvas to the DIV container. */
 	this.divContainer.appendChild(this.canvas);
 	
 	/* Fetch resize method of the DIV container and window. */
-	this.divContainer.onresize = function () { view.onResize(); };
-	window.onresize = function () { view.onResize(); };
+	this.divContainer.onresize = function () { canvasView.onResize(); };
+	window.onresize = function () { canvasView.onResize(); };
 	
 	/* Create a Animator listener to adaptor events. */
-	this.animatorListener = new AnimatorListener();
-	this.animatorListener.onAnimatorPause = function () { view.onViewPause(); }
-	this.animatorListener.onAnimatorResume = function () { view.onViewResume(); }
+	this.animatorListener = new AnimatorListener(this.onPause, this.onResume);
 	
 	/* Append the listener to the animator. */
 	this.animator.addAnimatorListener(this.animatorListener);
@@ -110,4 +129,25 @@ var CanvasView = function (divContainer) {
 	
 	/* Update the canvas dimensions to fit the given container. */
 	this.onResize();
+};
+
+/**  
+ *	CanvasViewListener.js is an interface object class which handles events triggered from CanvasView.
+ *	Each instance is required to be added into an canvas view object.
+ */
+var CanvasViewListener = function (onCanvasViewPause, onCanvasViewResume) {
+
+	/** Perform action for pause event */
+	if (onCanvasViewPause && typeof onCanvasViewPause === "function") {
+		this.onCanvasViewPause = onCanvasViewPause;
+	} else {
+		this.onCanvasViewPause = function() {};
+	}
+
+	/** Perform action for resume event */
+	if (onCanvasViewResume && typeof onCanvasViewResume === "function") {
+		this.onCanvasViewResume = onCanvasViewResume;
+	} else {
+		this.onCanvasViewResume = function() {};
+	}
 };
