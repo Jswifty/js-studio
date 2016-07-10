e0bd19cc6b3626efaa9a44542491387b409cce3f
Jswifty <wongjohnwwc@yahoo.com.hk>
2016-07-10 13:42:53 +0100

    minor fix in DOM Element

--- src/domelement/domelement.js
+++ src/domelement/domelement.js
@@ -1,39 +1,55 @@
 define([
-  "../classmanager/classmanager",
+  "../classmanager/classmanager"
 ],function (ClassManager) {
 
 	return function (properties) {
+    properties = properties || {};
 
 		var domElement = document.createElement(properties.type || "div");
-		domElement.id = properties.id;
-		domElement.className = properties.class;
+
+    if (properties.id !== undefined) {
+	    domElement.id = properties.id;
+    }
+
+    if (properties.class !== undefined) {
+      domElement.className = properties.class;
+    }
+
+    if (properties.html !== undefined) {
+      domElement.innerHTML = properties.html;
+    }
 
 		domElement.onMouseDown = function (mouseDown) {
 			domElement.addEventListener("mousedown", function (event) {
 				mouseDown(event);
 			}, false);
+      return domElement;
 		};
 
 		domElement.onMouseUp = function (mouseUp) {
 			domElement.addEventListener("mouseup", function (event) {
 				mouseUp(event);
 			}, false);
+      return domElement;
 		};
 
-    domElement.hasClass = function (classname)  {
+    domElement.hasClass = function (classname) {
       return ClassManager.hasClass(domElement, classname);
     };
 
-    domElement.addClass = function (classname)  {
+    domElement.addClass = function (classname) {
       ClassManager.addClass(domElement, classname);
+      return domElement;
     };
 
-    domElement.removeClass = function (classname)  {
+    domElement.removeClass = function (classname) {
       ClassManager.removeClass(domElement, classname);
+      return domElement;
     };
 
-    domElement.toggleClass = function (classname, toggleOn)  {
+    domElement.toggleClass = function (classname, toggleOn) {
       ClassManager.toggleClass(domElement, classname, toggleOn);
+      return domElement;
     };
 
 		return domElement;
