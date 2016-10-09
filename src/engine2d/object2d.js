0705fcd6b569d591852145baa45b8b233c49d961
Jswifty <wongjohnwwc@yahoo.com.hk>
2016-10-09 14:21:26 +0100

    added get and set for Object2D

--- src/engine2d/object2d.js
+++ src/engine2d/object2d.js
@@ -9,6 +9,38 @@ define(function () {
     object2D.height = height;
     object2D.centerX = x + width / 2;
     object2D.centerY = y + height / 2;
+
+    object2D.updateCenterPosition = function () {
+      object2D.centerX = object2D.x + object2D.width / 2;
+      object2D.centerY = object2D.y + object2D.height / 2;
+    };
+
+    object2D.getPosition = function () {
+      return { x: object2D.x, y: object2D.y };
+    };
+
+    object2D.setPosition = function (x, y) {
+      object2D.x = x || object2D.x;
+      object2D.y = y || object2D.y;
+
+      object2D.updateCenterPosition();
+    };
+
+    object2D.setDimension = function () {
+      return { width: object2D.width, height: object2D.height };
+    };
+
+    object2D.setDimension = function (width, height) {
+      object2D.width = width || object2D.width;
+      object2D.height = height || object2D.height;
+
+      object2D.updateCenterPosition();
+    };
+
+    object2D.getCenterPosition = function () {
+      return { x: object2D.centerX, y: object2D.centerY };
+    };
+
     object2D.render = function (context, x, y, width, height) {};
     object2D.update = function (timeDiff, width, height, speedX, speedY) {};
   };
